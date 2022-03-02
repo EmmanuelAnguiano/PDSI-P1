47d57c47f13fb60146e9e30b28c322a73c447f7b
EmmanuelAnguiano <canguiano12@ucol.mx>
2022-03-01 18:26:46 -0600

    actividad 3

--- act3/db/mysql.js
+++ act3/db/mysql.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '12345678',
-    database: 'prueba'
+    database: 'coneccion'
 });
 
 connection.connect(
--- act3/server.js
+++ act3/server.js
@@ -1,28 +1,3 @@
-/*require('dotenv').config()
-const express = require('express')
-const app=express()
-const { appConfig } = require('./lib/config')
-const connection = require('./db/mysql');
-
-
-app.get('/', function(req, res) {
-    res.sendfile('./views/index.html');    
-    let ts = Date.now();
-    connection.query('INSERT INTO coneccion (val, gety) VALUES (?,?)', [ts, 'raiz'])
-    console.log(ts);
-    });
-
-    app.get('/add', function(req, res) {
-        res.sendfile('./views/add.html');
-          let ts = Date.now();
-      
-          connection.query('INSERT INTO coneccion(val, gety) VALUES (?,?)', [ts, 'add'])
-      });
-
-app.listen(appConfig.port, ()=>
-console.log(`puesto en marcha en el puerto ${appConfig.port}`))*/
-
-
 require('dotenv').config()
 const express = require('express')
 const app = express()
@@ -32,14 +7,14 @@ const connection = require('./db/mysql');
 app.get('/', function(req, res) {
   res.sendfile('./views/index.html');
     let ts = Date.now();
-    connection.query('INSERT INTO coneccion(id, gety) VALUES (?,?)', [ts, 'raiz'])
+    connection.query('INSERT INTO prueba(id, gety) VALUES (?,?)', [ts, 'raiz'])
 }); 
 
 app.get('/add', function(req, res) {
   res.sendfile('./views/add.html');
     let ts = Date.now();
 
-    connection.query('INSERT INTO coneccion(id, gety) VALUES (?,?)', [ts, 'add'])
+    connection.query('INSERT INTO prueba(id, gety) VALUES (?,?)', [ts, 'add'])
 });
 
 app.listen(appConfig.port, ()=> console.log(`Puesto en marcha en puerto ${appConfig.port}`))
